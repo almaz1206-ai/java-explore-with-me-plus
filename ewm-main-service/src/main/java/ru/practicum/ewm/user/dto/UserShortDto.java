48bdefd5d3ad32a24e65e358bf4a1dda8bcef94b
Kirill2511 <kirill.kirill2@yandex.ru>
2026-02-26 01:10:43 +0700

    Implement user management features with error handling (#4)
    Co-authored-by: Кирилл Ларионов <k.larionov@isimplelab.com>

---
+++ ewm-main-service/src/main/java/ru/practicum/ewm/user/dto/UserShortDto.java
@@ -0,0 +1,13 @@
+package ru.practicum.ewm.user.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserShortDto {
+    private Long id;
+    private String name;
+}
