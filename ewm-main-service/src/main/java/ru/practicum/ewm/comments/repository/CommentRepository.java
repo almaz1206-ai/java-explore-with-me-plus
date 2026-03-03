4070c00c2285fedac553c22da9fd0e15246b89bf
Кирилл Ларионов <k.larionov@isimplelab.com>
2026-03-04 04:45:11 +0500

    fix Refactor comments, add postman test and add spec

--- ewm-main-service/src/main/java/ru/practicum/ewm/comments/repository/CommentRepository.java
+++ ewm-main-service/src/main/java/ru/practicum/ewm/comments/repository/CommentRepository.java
@@ -7,12 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import ru.practicum.ewm.comments.model.Comment;
 
-import java.util.List;
-
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findAllByAuthorId(Long userId, Pageable pageable);
-
-    List<Comment> findAllByEventId(Long eventId, Pageable pageable);
 
     @Query("""
        SELECT c
